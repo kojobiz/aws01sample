466b59602d072b3095ab5099eecbc6385cd2a7de
hera <hera@heranoMacBook-Pro.local>
2024-04-13 19:36:10 +0900

    test image

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ import Breadcrumbs from '@mui/material/Breadcrumbs';
 import { Link } from '@mui/icons-material';
 import Typography from '@mui/material/Typography';
 
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
 
- 
+// itemDataのimport文を追加
+import { itemData } from './ImageData.js';
 
 function App() {
   return (
@@ -52,6 +60,40 @@ function App() {
   </Link>
   <Typography color="text.primary">Breadcrumbs</Typography>
 </Breadcrumbs>
+
+
+
+    <ImageList sx={{ width: 500, height: 450 }}>
+      <ImageListItem key="Subheader" cols={2}>
+        <ListSubheader component="div">December</ListSubheader>
+      </ImageListItem>
+      {itemData.map((item) => (
+        <ImageListItem key={item.img}>
+          <img
+            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            src={`${item.img}?w=248&fit=crop&auto=format`}
+            alt={item.title}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title={item.title}
+            subtitle={item.author}
+            actionIcon={
+              <IconButton
+                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                aria-label={`info about ${item.title}`}
+              >
+                <InfoIcon />
+              </IconButton>
+            }
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+
+
+   
+
 
        
 
